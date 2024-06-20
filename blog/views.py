c337e177970ccc12a5e6d18349b8f2357e677f0b
Carlos Galiano <galiano111@gmail.com>
2024-06-20 18:26:59 -0500

    vista de clase en lista

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
 # creacion de vista de detalle y listado
@@ -24,6 +26,13 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
         
     return render(request, 'blog/post/list.html', { 'page': page, 'posts':posts })
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
